bc3db2322dd82255c2e47790f049825d2173006f
Luan Sena <luansena@live.com>
2017-08-19 00:43:05 -0300

    move arquivos para pasta raiz

--- backend/api/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-from api.resources.todo import todo_blueprint
-
-app = Flask(__name__)
-
-app.register_blueprint(todo_blueprint)
--- backend/api/resources/__init__.py
+++
@@ -1,2 +0,0 @@
-#contains all the controllers for every API endpoints, such as auth (login/singup)
-
