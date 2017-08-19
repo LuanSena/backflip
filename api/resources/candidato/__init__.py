f36ed6c3155949c3355a86448644112a5dc7e460
rmterra <r.mirandaterra@gmail.com>
2017-08-19 12:41:24 -0300

    Adicionar rota de POST e PUSH para candidatos

--- api/resources/candidato/__init__.py
+++ api/resources/candidato/__init__.py
@@ -10,6 +10,6 @@ candidato_blueprint = Blueprint("Candidato", __name__)
 app = restful.Api()
 app.init_app(candidato_blueprint)
 
-app.add_resource(CandidatosController, '/candidatos/<candidato_id>')
+app.add_resource(CandidatosController, '/candidatos', '/candidatos/<candidato_id>')
 app.add_resource(ListaCandidatosController, '/candidatos')
 app.add_resource(CandidatoStatusController, '/candidatos/<candidato_id>/status')
