6817776bec8e602da2b06f30fa02c573667e8127
Luan Sena <luansena@live.com>
2017-08-19 05:04:04 -0300

    Adiciona obs para alteração de status

--- api/db/pg_db_manager.py
+++ api/db/pg_db_manager.py
@@ -29,14 +29,24 @@ def insert_candidato(nome, idade, cidade, estado, area, subarea, tags, email, te
         conn.close()
 
 
-def update_candidato_status(id: int, status: int):
-    query = """
+def update_candidato_status(id: int, status: int, obs):
+    query_status = """
                 UPDATE candidato SET status = {status} where = {id};
+            """
+
+    query_obs = """
+                INSERT INTO 
+                        candidato_obs (candidato_id, obs)
+                VALUES 
+                    ({id}, "Novo status:{status}, {obs}");
             """
     try:
         conn = pg_connection.get_db_connection()
         cursor = conn.cursor()
-        cursor.execute(query.format(id=id, status=status))
+        cursor.execute(query_status.format(id=id, status=status))
+
+        cursor_obs = conn.cursor()
+        cursor_obs.execute(query_obs.format(id=id, status=status, obs=obs))
         return True
     except Exception as e:
         print(str(e))
