a29dd8aa64c1c1353d83e72c545bb441651e208e
rmterra <r.mirandaterra@gmail.com>
2017-08-20 07:12:49 -0300

    Remove controller não utilizada

--- api/resources/todo/controller.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-
-from api.resources.todo.model import Todo
-
-
-class HelloWorld(Resource):
-    def get(self):
-        todo = Todo
-        return todo.get_name()
