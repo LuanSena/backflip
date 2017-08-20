f085db9a75fe4220e3c9c3828254ce31ad10c5ca
rmterra <r.mirandaterra@gmail.com>
2017-08-20 10:28:59 -0300

    Atualiza template do corpo do email de confirmação de curriculos

--- api/common/email.py
+++ api/common/email.py
@@ -1,13 +1,10 @@
 import smtplib
 
 def send_email(user, pwd, recipient, subject, body):
-
-    print(user)
-    print(pwd)
-
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (user, ", ".join(recipient), subject, body)
+    """ % (user, recipient, subject, body)
+
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.ehlo()
