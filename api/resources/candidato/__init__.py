e8eb20d6535ec2c2e33003bbddd535c1e2fa3966
rmterra <r.mirandaterra@gmail.com>
2017-08-20 01:20:42 -0300

    Adiciona a rota necessaria para realizar a atualização do candidato

--- api/resources/candidato/__init__.py
+++ api/resources/candidato/__init__.py
@@ -11,7 +11,7 @@ candidato_blueprint = Blueprint("Candidato", __name__)
 app = restful.Api()
 app.init_app(candidato_blueprint)
 
-app.add_resource(CandidatosController, '/candidatos')
+app.add_resource(CandidatosController, '/candidatos', '/candidatos/<candidato_id>')
 app.add_resource(GetCandidatoController, '/candidatos/<candidato_id>')
 app.add_resource(ListaCandidatosController, '/candidatos')
 app.add_resource(CandidatoStatusController, '/candidatos/<candidato_id>/status')
